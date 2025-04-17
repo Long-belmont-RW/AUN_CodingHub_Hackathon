45587bae9ba3441384d1bfae1b94da86aebb8dc8
bolajiamir <bolajiamir@gmail.com>
2025-04-17 19:23:42 +0100

    Update amir_bolaji-abdullahi_task_2.py
    Task 2

--- Round_2/amir_bolaji-abdullahi_task_2.py
+++ Round_2/amir_bolaji-abdullahi_task_2.py
@@ -1,10 +1,11 @@
-def FooBar(n): 
-    for i in range(n): 
+def fizzbuzz(n): 
+    for i in range(1,n): 
         if i % 15 == 0 : 
-            print("FooBar",end="")
+            print("FooBar", end="")
         elif i % 3 == 0: 
-            print("Foo",end="")
+            print("Foo",end="",)
         elif i % 5 == 0: 
             print("Bar",end="")
         else: 
-            print(n,end="")
+            print(i,end="")
+
